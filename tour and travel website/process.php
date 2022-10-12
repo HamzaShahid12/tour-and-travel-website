dacd66b6bdb4346a0bd7f635c285ae2f51d887b3
Hamza Shahid <94144470+HamzaShahid12@users.noreply.github.com>
2022-10-12 14:46:36 +0500

    Add files via upload

---
+++ tour and travel website/process.php
@@ -0,0 +1,20 @@
+<?php
+include_once 'database.php';
+if(isset($_POST['save']))
+{	 
+	 $first_name = $_POST['first_name'];
+	 $last_name = $_POST['last_name'];
+	 $city_name = $_POST['city_name'];
+	 $email = $_POST['email'];
+	 @$sex = $_POST['sex'];
+	 $sql = "INSERT INTO employee (first_name,last_name,city_name,email,sex)
+	 VALUES ('$first_name','$last_name','$city_name','$email','$sex')";
+	 if (mysqli_query($conn, $sql)) {
+		echo "New record created successfully !";
+	 } else {
+		echo "Error: " . $sql . "
+" . mysqli_error($conn);
+	 }
+	 mysqli_close($conn);
+}
+?>
